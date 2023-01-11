00f872ab37085ef86c2850b7ae1b0650b6203863
Marlon Subuyú <marlonsubuyu.2012@gmail.com>
2023-01-10 22:29:30 -0400

    floating nav component CSS

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 import './header.css'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Marlon Subuyú</h1>
+        <h5 className='text-light'>Software Engineer</h5>
+        <CTA/>
+        <HeaderSocial/>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'> Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
